3588bef23681da1eb495c735c0c827c9fd7cd970
Jack Diver <diverj@google.com>
2024-04-25 16:27:19 +0100

    Add goto declaration/definition key bindings
    Signed-off-by: Jack Diver <diverj@google.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,4 +27,16 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-require'lspconfig'.clangd.setup{}
+local custom_attach = function(client, bufnr)
+  print("LSP started.");
+
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+end
+
+require'lspconfig'.clangd.setup{
+  init_options = { semanticHighlighting = true },
+  on_attach=custom_attach,
+  flags = { debounce_text_changes = 150 }
+}
